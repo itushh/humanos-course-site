715e3f38b30efa0bedcac30327f98a0659b1a9e3
itushh <tusharramgirkar99@gmail.com>
2026-01-31 01:33:35 +0530

    ui fix

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -4,9 +4,9 @@ import Monk from "@/assets/monk.png";
 const Hero = () => {
   return (
     <div className="md:mt-40 -mb-80 md:mb-0 w-full scale-30 sm:scale-40 md:scale-50 lg:scale-75 xl:scale-90 flex justify-center">
-      <div className="font-jomhuria w-fit mx-auto text-primary/80 relative">
+      <div className="font-jomhuria w-fit mx-auto dark:text-primary/90 relative">
         <h2 className="text-left text-7xl pl-4">Bring</h2>
-        <h2 className="text-[700px] -mt-82 -mb-108">PEACE</h2>
+        <h2 className="text-[700px] -mt-82 -mb-108 text-transparent dark:text-primary/90 [-webkit-text-stroke:2px_black]">PEACE</h2>
         <h2 className="text-right text-7xl pr-2">in Life</h2>
         <div className="absolute -top-24 left-90">
           <img className="w-140" src={Monk} />
